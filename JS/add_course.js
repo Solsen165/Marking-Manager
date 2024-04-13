b84271110978050c25a681a297e62139a73ce258
Solsen165 <islammohsen165@gmail.com>
2024-04-14 00:36:00 +0800

    Implemented the add student function

--- JS/add_course.js
+++ JS/add_course.js
@@ -1,22 +1,22 @@
 const form = document.querySelector('#addCourseForm');
 const msg = document.querySelector('#msg');
+const url = 'https://marking-manager-api.vercel.app';
 
 form.addEventListener('submit', (e) => {
    e.preventDefault();
    const formData = new FormData(form);
-   const formData2 = new URLSearchParams(formData).toString()
-   console.log(form.body)
-   console.log([...formData])
-   fetch('http://localhost:3000/courses', {
+   const encoded = new URLSearchParams(formData).toString()
+   fetch(url + '/courses', {
     method:'POST',
-    body: formData2,
+    body: encoded,
     headers: {
         'Content-type': 'application/x-www-form-urlencoded'
     }
    })
    .then((res) => {
     if (res.ok) {
-        msg.innerHTML = "Course created successfully";
+        msg.innerHTML = "Course created successfully: ";
+        res.json().then((data) => {msg.innerHTML += `Id: ${data.id}, Title: ${data.name}, Credits: ${data.credits}`})
     }
     else {
         msg.innerHTML = "Something went wrong, the course was not created";
